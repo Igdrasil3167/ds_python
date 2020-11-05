bebc812dd2efc559c0a81ce6c0c42efd8ab26d26
Igdrasil3167 <tima-slepcov@mail.ru>
2020-11-05 20:09:52 +0900

    модифицировали файл 1.NumPy

--- calc.py
+++ calc.py
@@ -1,17 +1,11 @@
 while True:
-
     try:
         a = int (input("Введите первое число:"))
         b = int (input("Введите второе число:"))
-        type
         operation =input("ВВедите символ операции(+,-,*,/,^):")
-
-            except ValueError:
-                    print("Нужно ввести число!")
-                    break
-            except ZeroDivisionError:
-                print("Вы попытались поделить на ноль!")
-                break
+    except ValueError:
+        print("Нужно ввести число!")
+        break
         if operation == "+":
             res = a + b
         elif operation == "-":
@@ -19,10 +13,9 @@ while True:
         elif operation == "*":
             res = a * b
         elif operation == "/":
-            res = a / b
+            res = a / b             
         elif operation == "^":
             res = a ** b
         else:
             res ("Символ операции некорректный")
-
-    print(f"Результат: {res}")
+        print(f"{res}")
